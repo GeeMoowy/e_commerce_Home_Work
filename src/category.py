9d278090033749583f39713eda560978a31c68e3
geemoowy <kovylek.ul@gmail.com>
2025-01-09 18:58:11 +0300

    checking by linters

--- src/category.py
+++ src/category.py
@@ -1,6 +1,5 @@
-
-
 class Category:
+    """Класс для представления категории товаров"""
     name: str
     description: str
     products: list
@@ -8,6 +7,7 @@ class Category:
     product_count = 0
 
     def __init__(self, name, description, products):
+        """Инициализация экземпляра класса категории товара. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.products = products
--- src/product.py
+++ src/product.py
@@ -1,12 +1,12 @@
-
-
 class Product:
+    """Класс для представления продуктов"""
     name: str
     description: str
     price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        """Инициализация экземпляра класса продуктов. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.price = price
